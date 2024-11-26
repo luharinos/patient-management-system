63972baf8d04aeb39f52de86148b332af4f3d787
luharinos <rahulvsoni1997@gmail.com>
2024-11-26 18:54:52 +0530

    update app error

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,6 +4,7 @@ import { AppDataSource } from '../config/database'
 import { User } from '../entities/User'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import { AppError } from '../middlewares/errorHandler'
 
 const userRepository: Repository<User> = AppDataSource.getRepository(User)
 
@@ -17,18 +18,16 @@ export async function login(
 		const user = await userRepository.findOne({ where: { email } })
 
 		if (!user || !(await bcrypt.compare(password, user.password))) {
-			res.status(401).send('Invalid credentials')
-			// return
+			throw new AppError('Invalid email or password', 401)
 		}
 		const token = jwt.sign(
 			{ id: user?.id, role: user?.role },
-			'your_jwt_secret',
+			process.env.JWT_SECRET as string,
 			{
 				expiresIn: '1h'
 			}
 		)
 		res.json({ token })
-		// return
 	} catch (err) {
 		next(err)
 	}
@@ -40,25 +39,26 @@ export async function register(
 	next: NextFunction
 ): Promise<void> {
 	try {
-		const { name, email, password, role } = req.body
+		const { name, email, password, role, contactNumber, age, gender } = req.body
 		const user = await userRepository.findOne({ where: { email } })
 		if (user) {
-			res.status(409).json({ message: 'Email already exists' })
-			// return
+			throw new AppError('Email already exists', 409)
 		} else {
 			if (!name || !email || !password || !role) {
-				throw new Error('All fields are required')
+				throw new AppError('Name, Email, Password and Role are required', 400)
 			}
 			const hashedPassword = await bcrypt.hash(password, 10)
 			const newUser = userRepository.create({
 				name,
 				email,
 				password: hashedPassword,
-				role
+				role,
+				contactNumber,
+				age,
+				gender
 			})
 			await userRepository.save(newUser)
 			res.status(201).json(newUser)
-			// return
 		}
 	} catch (err) {
 		next(err)
