fcebf8a3e3e2ee8b1095e58570dd09d71f96a82b
luharinos <rahulvsoni1997@gmail.com>
2024-11-26 16:38:36 +0530

    init server and linter

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPlugin from '@typescript-eslint/eslint-plugin'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import parser from '@typescript-eslint/parser'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{
+		languageOptions: {
+			ecmaVersion: 2021,
+			sourceType: 'module',
+			parser: parser
+		}
+	},
+	{
+		rules: {
+			'prettier/prettier': 'error',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'no-console': 'warn'
+		}
+	},
+	{
+		plugins: {
+			'@typescript-eslint': eslintPlugin,
+			'prettier': eslintPluginPrettier
+		}
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended
+]
